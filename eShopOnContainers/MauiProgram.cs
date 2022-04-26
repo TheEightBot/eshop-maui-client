89b8507c15192c83defcc95ccc254e636ea3504e
Michael Stonis <michael.stonis@eightbot.com>
2022-04-25 20:18:17 -0500

    migrate the fonts files

--- eShopOnContainers/MauiProgram.cs
+++ eShopOnContainers/MauiProgram.cs
@@ -19,6 +19,13 @@ public static class MauiProgram
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+
+                    fonts.AddFont("Font_Awesome_5_Free-Regular-400.otf", "FontAwesome-Regular");
+                    fonts.AddFont("Font_Awesome_5_Free-Solid-900.otf", "FontAwesome-Solid");
+                    fonts.AddFont("Montserrat-Bold.ttf", "Montserrat-Bold");
+                    fonts.AddFont("Montserrat-Regular.ttf", "Montserrat-Regular");
+                    fonts.AddFont("SourceSansPro-Regular.ttf", "SourceSansPro-Regular");
+                    fonts.AddFont("SourceSansPro-Solid.ttf", "SourceSansPro-Solid");
                 });
 
         return builder.Build();
