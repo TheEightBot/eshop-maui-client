ae7e6e63e67deee527e1c93c4db67de41a0a5299
Michael Stonis <michael.stonis@eightbot.com>
2022-05-05 21:32:44 -0500

    add MVVM Toolkit components and migrate the Login View Model

--- eShopOnContainers/Validations/ValidatableObject.cs
+++ eShopOnContainers/Validations/ValidatableObject.cs
@@ -1,64 +1,56 @@
-﻿using eShopOnContainers.ViewModels.Base;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using eShopOnContainers.ViewModels.Base;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace eShopOnContainers.Validations
 {
-    public class ValidatableObject<T> : ExtendedBindableObject, IValidity
+    public class ValidatableObject<T> : ObservableObject, IValidity
     {
         private readonly List<IValidationRule<T>> _validations;
-		private List<string> _errors;
+		private IEnumerable<string> _errors;
         private T _value;
         private bool _isValid;
 
         public List<IValidationRule<T>> Validations => _validations;
 
-		public List<string> Errors
+		public IEnumerable<string> Errors
         {
             get => _errors;
-            set
-            {
-                _errors = value;
-                RaisePropertyChanged(() => Errors);
-            }
+            private set => SetProperty(ref _errors, value);
         }
 
         public T Value
         {
             get => _value;
-            set
-            {
-                _value = value;
-                RaisePropertyChanged(() => Value);
-            }
+            set => SetProperty(ref _value, value);
         }
 
         public bool IsValid
         {
             get => _isValid;
-            set
-            {
-                _isValid = value;
-                RaisePropertyChanged(() => IsValid);
-            }
+            private set => SetProperty(ref _isValid, value);
         }
 
         public ValidatableObject()
         {
             _isValid = true;
-            _errors = new List<string>();
+            _errors = Enumerable.Empty<string>();
             _validations = new List<IValidationRule<T>>();
         }
 
         public bool Validate()
         {
-            Errors.Clear();
+            var errors =
+                _validations
+                    ?.Where(v => !v.Check(Value))
+                    ?.Select(v => v.ValidationMessage)
+                    ?.ToArray()
+                    ?? Enumerable.Empty<string>();
 
-            IEnumerable<string> errors = _validations.Where(v => !v.Check(Value))                             
-                .Select(v => v.ValidationMessage);
+            Errors = errors;
 
-			Errors = errors.ToList();
-            IsValid = !Errors.Any();
+            IsValid = !errors.Any();
 
             return this.IsValid;
         }
